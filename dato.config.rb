51ffcc96b686acdab22fc87d2859b7b69a9fdae5
Serhii Panov <spanov@integrity.com.ua>
2016-09-22 11:02:07 +0300

    DatoCMS : mv `NEWS` content to DatoCMS

--- dato.config.rb
+++ dato.config.rb
@@ -1,11 +1,13 @@
-dato.available_locales.each do |locale|
-  directory "content/#{locale}" do
-    I18n.with_locale(locale) do
-      create_data_file "site.yml", :yaml, dato.site.to_hash
-      dato.item_types.each do |item_type|
-        create_data_file "#{item_type.api_key}.yml", :yaml,
-                         dato.items_of_type(item_type).map(&:to_hash)
-      end
+# bundle exec dato dump --token=d28e9f0f1bcdd5e1dee5b6f2d78bce375e162b9a0466def110
+
+directory "_news" do
+  dato.news.each_with_index do |element, i|
+    create_post "#{element.slug}.md" do
+      frontmatter :yaml,
+                  layout: 'post',
+                  title: element.title,
+                  categories: 'news'
+      content element.description
     end
   end
 end
